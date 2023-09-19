62495408a979c87a7fc6756ea4acdd60006c0428
SiyeonL <leesi@dickinson.edu>
2023-09-18 20:11:39 -0400

    SiyeonLee HW 1

--- 1st script.R
+++ 1st script.R
@@ -1,3 +1,14 @@
 library("MASS")
 head(Cars93)
 table(Cars93$AirBags)/nrow(Cars93)
+mean (Cars93$MPG.city)
+mean (Cars93$MPG.city,trim=.1)
+median (Cars93$MPG.city)
+table (Cars93$Cylinders)
+
+table (Cars93$AirBags)
+
+#create your own data frame
+price_data<-data.frame(size_sq_ft=c(2521,2555,2735,2846,3028,3049,3198,3198),
+                       selling_price = c(400,426,428,435,469,475,488,455)
+                       
